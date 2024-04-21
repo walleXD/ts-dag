d90eb8d8f4b6a66b69455fe4abff5c5406c6449c
Walee Ahmed <6826840+walleXD@users.noreply.github.com>
2024-04-21 16:43:59 -0400

    chore(builder): coverage setup

--- pkgs/builder/src/main.ts
+++ pkgs/builder/src/main.ts
@@ -1,27 +1,2 @@
+/* v8 ignore next 3 */
 export * from "./dag";
-
-/**
-// Example usage
-import { createDag } from 'ts-dag';
-
-const dag = createDag()
-  .context({})
-  .task('task1', async (ctx) => {
-    // Task 1 implementation
-    console.log('Running Task 1');
-  })
-  .dependsOn((ctx) => [])
-  .task('task2', async (ctx) => {
-    // Task 2 implementation
-    console.log('Running Task 2');
-  })
-  .dependsOn((ctx) => [ctx.tasks.task1])
-  .task('task3', async (ctx) => {
-    // Task 3 implementation
-    console.log('Running Task 3');
-  })
-  .dependsOn((ctx) => [ctx.tasks.task1, ctx.tasks.task2]);
-
-dag.run();
-
-*/
--- pkgs/builder/vitest.config.ts
+++ pkgs/builder/vitest.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
   test: {
     testTimeout: 50000,
     includeSource: ["src/**/*.{js,ts}"],
+    coverage: {
+      provider: "v8", // or 'v8'
+      reporter: ["text", "json", "html"],
+    },
   },
 });
